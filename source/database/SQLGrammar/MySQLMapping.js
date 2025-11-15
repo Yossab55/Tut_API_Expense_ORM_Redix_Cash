a72dbaa8cefac279aa68f2957881f35928307b5a
Yossab_Samuel <yossabpro5@gmail.com>
2025-11-15 10:02:13 +0200

    delete the self-build orm and start with Knex

--- source/database/SQLGrammar/MySQLMapping.js
+++
@@ -1,9 +0,0 @@
-import { MySQLGrammar } from "./MySQLGrammar.js";
-
-const MySQLMapping = {
-  select: MySQLGrammar.buildSelect,
-  orderBy: MySQLGrammar.buildGroupBy,
-  groupBy: MySQLGrammar.buildGroupBy,
-};
-
-export { MySQLMapping };
